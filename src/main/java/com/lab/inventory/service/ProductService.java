d0be648f2f187136e1214de4acda4d055cc9e734
kevinfjq <109086639+kevinfjq@users.noreply.github.com>
2025-11-18 17:19:50 -0300

    feat: implement two-level caching (Caffeine L1 + Redis L2)
    - L1 Cache (Caffeine): Local in-memory caching for near-instant access and reduction of network traffic.
    - L2 Cache (Redis with Redisson): Distributed cache for data sharing between instances and persistence.
    - Implemented the Cache-Aside pattern (look L1 -> look L2 -> fetch DB -> populate L2/L1).
    - Configured L1 to expire after 1 minute (`expireAfterWrite`).
    - Configured L2 to expire after 10 minutes (TTL).

--- src/main/java/com/lab/inventory/service/ProductService.java
+++ src/main/java/com/lab/inventory/service/ProductService.java
@@ -1,10 +1,14 @@
 package com.lab.inventory.service;
 
+import java.time.Duration;
 import java.util.Optional;
 
+import org.redisson.api.RBucket;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.github.benmanes.caffeine.cache.Cache;
 import com.lab.inventory.dto.CreateProductRequestDTO;
 import com.lab.inventory.model.Product;
 import com.lab.inventory.repository.ProductRepository;
@@ -15,13 +19,19 @@ import com.lab.inventory.repository.ProductRepository;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final RedissonClient redissonClient;
+    private final Cache<Long, Product> localCache;
 
-    ProductService(ProductRepository productRepository) {
+    ProductService(ProductRepository productRepository, RedissonClient redissonClient,
+            Cache<Long, Product> localCache) {
         this.productRepository = productRepository;
+        this.redissonClient = redissonClient;
+        this.localCache = localCache;
     }
 
     @Transactional
     public Long createProduct(CreateProductRequestDTO request) {
+
         Product product = Product.builder()
                 .name(request.name())
                 .price(request.price())
@@ -32,7 +42,33 @@ public class ProductService {
     }
 
     public Optional<Product>  getProductById(Long id) {
-        return productRepository.findById(id);
+        Product localProduct = localCache.getIfPresent(id);
+        if (localProduct != null) {
+            System.out.println("Caffeine Cache hit for product id: " + id);
+            return Optional.of(localProduct);
+        }
+        RBucket<Product> redisBucket = redissonClient.getBucket("product:"+id);
+        Product redisProduct = redisBucket.get();
+
+        if(redisProduct != null) {
+            System.out.println("Redis Cache hit for product id: " + id);
+            localCache.put(id, redisProduct);
+            return Optional.of(redisProduct);
+        }
+
+        System.out.println("Cache miss for product id: " + id);
+        System.out.println("Fetching product id " + id + " from database.");
+
+        Optional<Product> dbProductOpt  = productRepository.findById(id);
+        if(dbProductOpt.isPresent()) {
+            Product product = dbProductOpt.get();
+            redisBucket.set(product, Duration.ofMinutes(10));
+
+            localCache.put(id, product);
+
+        }
+
+        return dbProductOpt;
         
     }
 }
